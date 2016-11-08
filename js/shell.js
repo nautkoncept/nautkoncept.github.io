572e6f354cb458ed4571edd63c615aa6dc847550
Andree Ray <info@andreeray.se>
2016-11-08 20:57:21 +0100

    f

--- js/shell.js
+++ js/shell.js
@@ -1,16 +1,13 @@
 
 
 var windowWidth = window.innerWidth
-var windowHeight = window.innerHeight
 
 
 var sectionHeader   = document.getElementById("section_header"),
     sectinoVideo    = document.getElementById("section_video"),
     sectionMeta     = document.getElementById("section_meta")
 
-sectionHeader.style.height  = (windowHeight - sectinoVideo.offsetHeight) / 2
-sectionMeta.style.height    = (windowHeight - sectinoVideo.offsetHeight) / 2
-
-console.log(windowHeight - sectinoVideo.offsetHeight)
-
-// sectinoVideo.style.height = "50px";
+window.onresize = function(event) {
+sectionHeader.style.height  = (window.innerHeight - sectinoVideo.offsetHeight) / 2
+sectionMeta.style.height    = (window.innerHeight - sectinoVideo.offsetHeight) / 2
+}();
