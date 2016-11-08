f4d8937839ae4cad052181b3d376d710087f949e
Andree Ray <info@andreeray.se>
2016-11-08 21:49:30 +0100

    f

--- js/loader.js
+++ js/loader.js
@@ -2,7 +2,6 @@ $(document).ready(function() {
     $('body').bind('touchmove', function(e) {
         e.preventDefault()
     })
-
 })
 
 $("#spinner").css({
--- js/shell.js
+++ js/shell.js
@@ -1,13 +1,1 @@
 
-
-var windowWidth = window.innerWidth
-
-
-var sectionHeader           = document.getElementById("section_header"),
-    sectinoVideo            = document.getElementById("section_video"),
-    sectionVideoSelection   = document.getElementById("section_video_selection")
-
-window.onresize = function(event) {
-sectionHeader.style.height          = (window.innerHeight - sectinoVideo.offsetHeight) / 2
-sectionVideoSelection.style.height  = (window.innerHeight - sectinoVideo.offsetHeight) / 2
-}();
