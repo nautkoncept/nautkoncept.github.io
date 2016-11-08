6ec3de5485be2b6faa8905f7f18ee6483d426791
Andree Ray <info@andreeray.se>
2016-11-08 21:10:40 +0100

    f

--- js/shell.js
+++ js/shell.js
@@ -3,11 +3,11 @@
 var windowWidth = window.innerWidth
 
 
-var sectionHeader   = document.getElementById("section_header"),
-    sectinoVideo    = document.getElementById("section_video"),
-    sectionMeta     = document.getElementById("section_meta")
+var sectionHeader           = document.getElementById("section_header"),
+    sectinoVideo            = document.getElementById("section_video"),
+    sectionVideoSelection   = document.getElementById("section_video_selection")
 
 window.onresize = function(event) {
-sectionHeader.style.height  = (window.innerHeight - sectinoVideo.offsetHeight) / 2
-sectionMeta.style.height    = (window.innerHeight - sectinoVideo.offsetHeight) / 2
+sectionHeader.style.height          = (window.innerHeight - sectinoVideo.offsetHeight) / 2
+sectionVideoSelection.style.height  = (window.innerHeight - sectinoVideo.offsetHeight) / 2
 }();
