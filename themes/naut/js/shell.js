8f055c547812534f3d4a2700735e2b67267b8a55
Andree Ray <info@andreeray.se>
2016-11-15 21:14:57 +0100

    f

--- themes/naut/js/shell.js
+++
@@ -1,4 +0,0 @@
-// var frame = document.getElementById('section_video').offsetHeight;
-
-
-document.getElementById('player-top').style.height = (window.innerHeight - document.getElementById('videoWrapper').offsetHeight ) / 2 + 'px'
