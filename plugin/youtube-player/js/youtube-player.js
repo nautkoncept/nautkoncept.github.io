7db1f14bb6e7f5b54db405355bb82dfa84275450
Andree Ray <info@andreeray.se>
2016-11-15 21:12:29 +0100

    f

---
+++ plugin/youtube-player/js/youtube-player.js
@@ -0,0 +1,57 @@
+// 2. This code loads the IFrame Player API code asynchronously.
+var tag = document.createElement('script');
+
+tag.src = "https://www.youtube.com/iframe_api";
+var firstScriptTag = document.getElementsByTagName('script')[0];
+firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
+
+// 3. This function creates an <iframe> (and YouTube player) after the API code downloads.
+
+
+
+var player;
+
+function onYouTubeIframeAPIReady(id) {
+    player = new YT.Player('player', {
+        height: '349',
+        width: '560',
+        videoId: 'b-3UgtZ0Gek',
+        suggestedQuality: 'highres',
+        playerVars: {
+            modestbranding: 1,
+            controls: 0,
+            disablekb: 1,
+            showinfo: 0,
+            nologo: 1,
+            playsinline: 1
+        },
+        events: {
+            'onReady': onPlayerReady,
+            'onStateChange': onPlayerStateChange
+        }
+    });
+}
+
+function loadVideo(id){
+    document.getElementById('youtube-player').style.display = "block"
+    document.getElementById('youtube-player').style.top = "0"
+    player.loadVideoById(id,0,'highres')
+
+}
+
+function onPlayerReady(event) {
+   event.target.stopVideo();
+}
+
+var done = false;
+function onPlayerStateChange(event) {
+    if (event.data == 0){
+        player.seekTo(0)
+        player.stopVideo()
+    }
+}
+
+function removePlayer(){
+    document.getElementById('youtube-player').style.display = "none"
+    player.stopVideo()
+}
---
+++ themes/naut/js/shell.js
@@ -0,0 +1,4 @@
+// var frame = document.getElementById('section_video').offsetHeight;
+
+
+document.getElementById('player-top').style.height = (window.innerHeight - document.getElementById('videoWrapper').offsetHeight ) / 2 + 'px'
