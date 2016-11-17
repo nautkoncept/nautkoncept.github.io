e7a10565f57c727076b0c70166489c1a818d1036
Andree Ray <info@andreeray.se>
2016-11-17 16:18:38 +0100

    f

--- assets/themes/naut/js/shell.js
+++ assets/themes/naut/js/shell.js
@@ -1,4 +1,17 @@
 // var frame = document.getElementById('section_video').offsetHeight;
 
+$(document).ready(function(){
+    document.getElementById('player-top').style.height = (window.innerHeight - document.getElementById('videoWrapper').offsetHeight ) / 2 + 'px'
+    if ($( window ).width() >="900") {
+        document.getElementById('shadow-bottom').style.height = $('footer').height()
+        document.getElementById('footer').style.height = $('#shadow-bottom').height()
+    }
 
-document.getElementById('player-top').style.height = (window.innerHeight - document.getElementById('videoWrapper').offsetHeight ) / 2 + 'px'
+    var page = $(window);
+   $(window).mousewheel(function(event, delta, deltaX, deltaY){
+       if (delta < 0) page.scrollTop(page.scrollTop() + 5);
+       else if (delta > 0) page.scrollTop(page.scrollTop() - 5);
+       return false;
+   })
+
+})
