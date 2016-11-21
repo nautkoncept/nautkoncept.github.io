26c9e8c3d0341149c7ff0fea2a169cb8a95f12c2
Andree Ray <info@andreeray.se>
2016-11-21 16:58:48 +0100

    f

--- assets/themes/naut/js/shell.js
+++ assets/themes/naut/js/shell.js
@@ -1,17 +1,22 @@
 // var frame = document.getElementById('section_video').offsetHeight;
 
-$(document).ready(function(){
-    document.getElementById('player-top').style.height = (window.innerHeight - document.getElementById('videoWrapper').offsetHeight ) / 2 + 'px'
-    if ($( window ).width() >="900") {
-        document.getElementById('shadow-bottom').style.height = $('footer').height()
+$(document).ready(function() {
+    document.getElementById('player-top').style.height = (window.innerHeight - document.getElementById('videoWrapper').offsetHeight) / 2 + 'px'
+    if ($(window).width() >= "900") {
+        document.getElementById('shadow-bottom').style.height = ($('#footer h2').height() *2) + $('#insta-img').height()
         document.getElementById('footer').style.height = $('#shadow-bottom').height()
-    }
 
-    var page = $(window);
-   $(window).mousewheel(function(event, delta, deltaX, deltaY){
-       if (delta < 0) page.scrollTop(page.scrollTop() + 5);
-       else if (delta > 0) page.scrollTop(page.scrollTop() - 5);
-       return false;
-   })
+    }
+    document.getElementById('insta').style.height = $('#footer h2').height()  + $('#insta-img').height()
+
+    var userFeed = new Instafeed({
+        get: 'user',
+        userId: '3624623948',
+        accessToken: '3624623948.1677ed0.d2cfa5afe2644bdfa7cca203e427a089',
+        template: '<img src="{{image}}"  />',
+        resolution: 'standard_resolution'
+    });
+    userFeed.run();
+
 
 })
