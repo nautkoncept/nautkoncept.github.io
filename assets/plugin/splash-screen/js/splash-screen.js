a44a4c1b629c32ca1ba0a6af35e8062b07fad07a
Andree Ray <info@andreeray.se>
2016-11-22 13:08:24 +0100

    f

---
+++ assets/plugin/splash-screen/js/splash-screen.js
@@ -0,0 +1,23 @@
+$(window).on('load', function(e) {
+    $('body').bind('touchmove', function(e) {
+        e.preventDefault()
+    })
+
+    $.when($("#loader_stage")
+        .delay(0)
+        .fadeOut(300))
+        .done(function() {
+            $('body').css('overflow', 'auto')
+            $('body').unbind('touchmove')
+    })
+})
+$("#spinner").css({
+    '-webkit-animation-name': 'spinner',
+    '-webkit-animation-timing-function': 'linear',
+    '-webkit-animation-iteration-count': 'infinite',
+    '-webkit-animation-duration': '2s',
+    'animation-name': 'spinner',
+    'animation-timing-function': 'linear',
+    'animation-iteration-count': 'infinite',
+    'animation-duration': '2s'
+});
